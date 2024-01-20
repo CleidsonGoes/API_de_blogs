7bc0b6dba77ee53ecfbb2be31d138ee3574147d5
CleidsonGoes <jorgegoes4@gmail.com>
2024-01-20 01:49:22 -0300

    req04 em andamento

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,8 @@
+const jwt = require('jsonwebtoken');
 const userService = require('../services/user.service');
+const { User } = require('../models');
 
-// function validateField(res, displayName, email, password) {
-// }
-
-async function createUserController(req, res) {
-  const { displayName, email, password } = req.body;
-  
+function validateField(res, displayName, email, password) {
   const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   
   if (displayName.length < 8) {
@@ -21,9 +18,25 @@ async function createUserController(req, res) {
       message: '"password" length must be at least 6 characters long',
     });
   }
+}
+
+async function createUserController(req, res) {
+  const secret = process.env.JWT_SECRET || 'seusecretdetoken';
+  const jwtConfig = { expiresIn: '7d', algorithm: 'HS256' };
+
+  const { displayName, email, password } = req.body;
+  validateField(res, displayName, email, password);
   
-  const returnUserService = await userService.userCreateService(res, req.body);
-  return returnUserService;
+  const returnUserService = await userService.userCreateService(email);
+  console.log('bem aquiiiiiii', returnUserService);
+
+  if (returnUserService && returnUserService === email) {
+    return res.status(409).json({ message: 'User already registered' });
+  }
+  await User.create(req.body);
+  const tokenCreated = jwt.sign({ data: { userId: returnUserService } }, secret, jwtConfig);
+  
+  return res.status(201).json({ token: tokenCreated });
 }
 
 module.exports = {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,27 +1,10 @@
-const jwt = require('jsonwebtoken');
-const userModel = require('../models/User');
+const { User } = require('../models');
 
-const secret = process.env.JWT_SECRET || 'seusecretdetoken';
-
-async function userCreateService(res, user) {
-  try {
-    const jwtConfig = { expiresIn: '7d', algorithm: 'HS256' };
-
-    const foundEmail = await userModel
-      .findOne({ where: { displayName: user.displayName, email: user.email } });
-    console.log('estou aqui', foundEmail);
-
-    if (foundEmail.dataValues.email === user.email) {
-      return res.status(409).json({ message: 'User already registered' });
-    }
-    await userModel.create(user);
-
-    const token = jwt.sign({ data: { userId: foundEmail.dataValues.id } }, secret, jwtConfig);
-
-    return res.status(201).json({ token });
-  } catch (err) {
-    return err.message;
-  }
+async function userCreateService(emailRequest) {
+  const foundForEmail = await User
+    .findOne({ where: { email: emailRequest } });
+  console.log('Olha aqui', foundForEmail);
+  return foundForEmail;
 }
 
 module.exports = {
